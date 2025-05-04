10da7e28881b0aecfdd40099096204044ff0c9a8
tsmr <r.l.moret@student.rug.nl>
2025-05-05 00:58:26 +0200

    improved organization of trainer and removed TrainerConfig

--- rlms_mlr/callbacks/eval_callback.py
+++ rlms_mlr/callbacks/eval_callback.py
@@ -3,30 +3,38 @@ import torch
 
 from rlms_mlr.callbacks.base_callback import Callback, TrainerState
 from rlms_mlr.data.dataset import Batch
-from rlms_mlr.models.base_model import Metrics
+from rlms_mlr.models.base_model import Metrics, Model
 
 
-class EvalCallback(Callback):
+def evaluate_model(model: Model, loader: Iterator[Batch]) -> Metrics:
+    model.eval()
+    metrics = None
+    for batch_idx, batch in enumerate(loader):
+        batch = batch.to(model.device, non_blocking=True)
+        with torch.no_grad():
+            new_metrics = model.evaluate(batch)
 
-    def __init__(self, eval_loader: Iterator[Batch]):
-        self.eval_loader = eval_loader
+        if metrics is None:
+            metrics = new_metrics
+        else:
+            metrics.merge(new_metrics)
+    return metrics
 
-    def on_epoch_end(self, trainer_state: TrainerState, **kwargs) -> None:
+class ValidateCallback(Callback):
+    def on_epoch_start(self, trainer_state: TrainerState, **kwargs) -> None:
         """
         Evaluate the model on the validation set at the end of each epoch.
         """
-        trainer_state.model.eval()
+        loader = trainer_state.data_module.val_dataloader()
+        metrics = evaluate_model(trainer_state.model, loader)
+        trainer_state.logs.update({f"val_{k}": v for k, v in metrics.compute_metrics().items()})
 
-        trainer_state.model.eval()
-        metrics = None
-        for batch_idx, batch in enumerate(self.eval_loader):
-            batch = batch.to(trainer_state.model.device, non_blocking=True)
-            with torch.no_grad():
-                new_metrics = trainer_state.model.evaluate(batch)
+class TestCallback(Callback):
+    def on_epoch_start(self, trainer_state: TrainerState, **kwargs) -> None:
+        """
+        Evaluate the model on the validation set at the end of each epoch.
+        """
+        loader = trainer_state.data_module.test_dataloader()
+        metrics = evaluate_model(trainer_state.model, loader)
 
-            if metrics is None:
-                metrics = new_metrics
-            else:
-                metrics.merge(new_metrics)
-
-        trainer_state.logs.update(metrics.compute_metrics())
+        trainer_state.logs.update({f"test_{k}": v for k, v in metrics.compute_metrics().items()})
--- rlms_mlr/trainer.py
+++ rlms_mlr/trainer.py
@@ -1,123 +1,16 @@
 from datetime import datetime
-from dataclasses import dataclass, asdict
 from pathlib import Path
 
 import torch
 from torch.amp import autocast
-from typing import Any, Optional, Callable, Union, Sequence, Iterable, List
 
-from rlms_mlr.callbacks.logging import LoggerCallback
-from rlms_mlr.callbacks.progress_bar import RichProgressCallback
+from rlms_mlr.callbacks.progress_bar_callback import RichProgressCallback
 from rlms_mlr.augmentation.augmentation_pipeline import AugmentationPipeline
 from rlms_mlr.callbacks.base_callback import Callback, TrainerState, CallbackList
-from rlms_mlr.callbacks.eval_callback import EvalCallback
+from rlms_mlr.callbacks.eval_callback import ValidateCallback, TestCallback
 from rlms_mlr.data.data_module import DataModule
 from rlms_mlr.data.dataset import Batch
-from rlms_mlr.loggers.base_logger import Logger
-from rlms_mlr.loggers.tensorboard_logger import TensorBoardLogger
 from rlms_mlr.models.base_model import Model
-
-
-@dataclass
-class ModelConfig:
-    _target_: str
-    _args_: dict = None
-    _kwargs_: dict = None
-
-@dataclass
-class TrainerConfig:
-    """
-    Configuration class for the Trainer. This class is used to store the configuration parameters for the Trainer.
-
-    Attributes:
-    """
-
-    # objects
-    model: Model
-    data_module: DataModule
-    device: str = "cuda"
-    augmentation_pipeline: AugmentationPipeline = None
-    lr_scheduler: torch.optim.lr_scheduler.LRScheduler = None
-    optimizer_cls: torch.optim.Optimizer = torch.optim.Adam
-
-    # training parameters
-    total_epochs: int = 10
-    use_amp: bool = False
-    gradient_clip_norm: float = None
-    optimizer_kwargs: dict = None
-    loader_kwargs: dict = None
-
-    # logging
-    logger: Logger = None
-    log_dir: Path = None
-
-    # callbacks
-    callbacks: List[Callback] = None
-    validate: bool = True
-    test: bool = False
-    progress_bar_callback = RichProgressCallback()
-
-    def __post_init__(self):
-        if self.test and self.validate:
-            # TODO: change the Logs propagation of callbacks to fix this issue
-            raise RuntimeError("Both test and validate cannot be True at the same time.")
-
-    def __getitem__(self, key: str) -> Any:
-        return getattr(self, key)
-
-    def keys(self):
-        return asdict(self).keys()
-
-    def generate_path(self) -> Path:
-        """
-        Generate a unique path based on the model name, dataset name, and current timestamp.
-
-        Returns: A Path object representing the generated path.
-        """
-        model = self.model.__class__.__name__
-        data_name = (self.data_module.train_dataset.__class__.__name__ or
-                    self.data_module.val_dataset.__class__.__name__ or
-                    self.data_module.test_dataset.__class__.__name__)
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        return Path(model, data_name, timestamp)
-
-    def get_trainer(self) -> "Trainer":
-        """
-        Create a Trainer instance using the configuration parameters.
-        Returns:
-            Trainer: An instance of the Trainer class.
-        """
-        self.data_module.dataloader_kwargs = self.loader_kwargs or {}
-
-        if self.optimizer_kwargs is None:
-            self.optimizer_kwargs = {}
-
-        # Create the optimizer
-        optimizer = self.optimizer_cls(self.model.parameters(), **self.optimizer_kwargs)
-
-        callbacks = self.callbacks or []
-        if self.validate:
-            callbacks.append(EvalCallback(self.data_module.val_dataloader()))
-        if self.test:
-            callbacks.append(EvalCallback(self.data_module.test_dataloader()))
-        self.log_dir = self.log_dir or self.generate_path()
-        logger = self.logger or TensorBoardLogger(self.log_dir)
-        log_callback = LoggerCallback(logger=logger, params=dict(self), log_dir=self.log_dir)
-        callbacks.append(log_callback)
-        if self.progress_bar_callback:
-            callbacks.append(self.progress_bar_callback)
-
-        return Trainer(
-            model=self.model,
-            optimizer=optimizer,
-            data_module=self.data_module,
-            total_epochs=self.total_epochs,
-            augmentation_pipeline=self.augmentation_pipeline,
-            lr_scheduler=self.lr_scheduler,
-            callbacks=CallbackList(callbacks),
-            use_amp=self.use_amp,
-            gradient_clip_norm=self.gradient_clip_norm,
-        )
 
 
 class Trainer:
@@ -126,45 +19,70 @@ class Trainer:
             model: Model,
             optimizer: torch.optim.Optimizer,
             data_module: DataModule,
-            total_epochs: int,
             augmentation_pipeline: AugmentationPipeline = None,
             lr_scheduler: torch.optim.lr_scheduler.LRScheduler = None,
-            callbacks: Callback = None,
-            use_amp: bool = False,
-            gradient_clip_norm: float = None,
+            callback: Callback = None,
     ):
+        """
+        Trainer class for training and evaluating models.
 
-        # Training objects
+        Args:
+            model:
+            optimizer:
+            data_module:
+            augmentation_pipeline:
+            lr_scheduler:
+            callback:
+        """
+
         self.model = model
-        self.optimizer = optimizer
+        # ensure the optimizer is created with the model parameters
+        self.optimizer = optimizer.__class__(params=model.parameters(), **optimizer.defaults)
         self.augmentation_pipeline = augmentation_pipeline
         self.lr_scheduler = lr_scheduler
-
-        # training configuration
-        self.total_epochs = total_epochs
-        self.use_amp = use_amp
-        self.gradient_clip_norm = gradient_clip_norm
-        self.scaler = torch.amp.GradScaler("cuda", enabled=use_amp)
-
-        # data
         self.data_module = data_module
         self.train_loader = data_module.train_dataloader()
-        self.val_loader = data_module.val_dataloader()
-        self.test_loader = data_module.test_dataloader()
-
-        self.callbacks = callbacks
+        self.callback = callback or self.default_callback()
 
         # internal state
         self.trainer_context = TrainerState(
             model=self.model,
             optimizer=self.optimizer,
             data_module=self.data_module,
-            total_epochs=self.total_epochs,
             total_training_batches=len(self.train_loader),
-            current_epoch=0,
-            current_batch=0,
             stop_training=False,
         )
+
+    def default_log_dir(self) -> Path:
+        model = self.model.__class__.__name__
+        data_name = (self.data_module.train_dataset.__class__.__name__ or
+                    self.data_module.val_dataset.__class__.__name__ or
+                    self.data_module.test_dataset.__class__.__name__)
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        return Path(model, data_name, timestamp)
+
+    def default_callback(self) -> CallbackList:
+        l = []
+        l.append(RichProgressCallback())
+        if self.data_module.val_dataset is not None and len(self.data_module.val_dataset):
+            l.append(ValidateCallback())
+        if self.data_module.test_dataset is not None and len(self.data_module.test_dataset):
+            l.append(TestCallback())
+        return CallbackList(l)
+
+
+    def add_callback(self, callback: Callback):
+        """
+        Add a callback to the trainer. If the trainer already has a callback list, add the new callback to it.
+
+        Args:
+            callback:
+        """
+        if isinstance(self.callback, CallbackList):
+            self.callback.add_callback(callback)
+        else:
+            self.callback = CallbackList([self.callback, callback])
+
     def _callback(self, method: str):
         """
         Call the callback method with the current trainer context. Process the returned CallbackReturn.
@@ -172,9 +90,9 @@ class Trainer:
             method: The callback method to call. This should be a method of the Callback class
                     (e.g. Callback.on_train_start).
         """
-        if self.callbacks is None:
+        if self.callback is None:
             return
-        callback_return = getattr(self.callbacks, method)(self.trainer_context)
+        callback_return = getattr(self.callback, method)(self.trainer_context)
         if callback_return is None:
             return
 
@@ -191,41 +109,55 @@ class Trainer:
         return batch
 
 
-    def train(self):
+    def train(self, total_epochs: int, use_amp: bool = False, gradient_clip_norm: float = None):
+
+        self.trainer_context.total_epochs = total_epochs
         self._callback("on_train_start")
 
-        for epoch in range(self.total_epochs):
+        scaler = torch.amp.GradScaler(self.model.device, enabled=use_amp)
+        for epoch in range(total_epochs):
+
             self.trainer_context.current_epoch = epoch
             self._callback("on_epoch_start")
-
             self.model.train()
+
+            metrics = None
             for batch_idx, batch in enumerate(self.train_loader):
                 self.trainer_context.current_batch = batch_idx
                 self._callback("on_train_batch_start")
-
-                loss = self.train_step(batch)
-                self.trainer_context.logs = {"train_loss": loss}
+                new_metrics = self.train_step(batch, use_amp=use_amp, scaler=scaler,
+                                            gradient_clip_norm=gradient_clip_norm)
+                if metrics is None:
+                    metrics = new_metrics
+                else:
+                    metrics.merge(new_metrics)
 
                 self._callback("on_train_batch_end")
 
+            self.trainer_context.logs.update({f"train_{k}": v for k, v in metrics.compute_metrics().items()})
             self._callback("on_epoch_end")
-
             if self.trainer_context.stop_training:
                 break
+
         self._callback("on_train_end")
 
-    def train_step(self, batch: Batch):
+    def train_step(
+            self,
+            batch: Batch,
+            use_amp: bool = False,
+            scaler: torch.amp.GradScaler = None,
+            gradient_clip_norm: float = None
+    ):
         processed_batch = self._prepare_batch(batch)
-        with autocast("cuda", enabled=self.use_amp):
-            loss = self.model.compute_loss(processed_batch)
-        self.scaler.scale(loss).backward()
-        if self.gradient_clip_norm:
-            self.scaler.unscale_(self.optimizer)
-            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.gradient_clip_norm)
-        self.scaler.step(self.optimizer)
-        self.scaler.update()
+        with autocast("cuda", enabled=use_amp):
+            loss, metrics = self.model.compute_loss(processed_batch)
+        scaler.scale(loss).backward()
+        if gradient_clip_norm:
+            scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), gradient_clip_norm)
+        scaler.step(self.optimizer)
+        scaler.update()
         self.optimizer.zero_grad()
         if self.lr_scheduler:
             self.lr_scheduler.step()
-
-        return loss.item()
+        return metrics
